fe5e42a2d7987372001d7081314e6644c49c1daf
elon <imcoding@foxmail.com>
2022-03-27 17:27:03 +0800

    init

---
+++ safety-admin/src/main/java/hust/software/elon/SafetyAdminApplication.java
@@ -0,0 +1,13 @@
+package hust.software.elon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyAdminApplication.class, args);
+	}
+
+}
--- safety-middle/src/main/java/hust/software/elon/SafetyMiddleApplication.java
+++ safety-middle/src/main/java/hust/software/elon/SafetyMiddleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AudioMiddleApplication {
+public class SafetyMiddleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AudioMiddleApplication.class, args);
+		SpringApplication.run(SafetyMiddleApplication.class, args);
 	}
 
 }
